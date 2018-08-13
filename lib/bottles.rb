d9846f8a53ff9dbb0ccf28d65ba14dd1503488f4
Sean DMR <sean@flyinggrizzly.io>
2018-08-13 15:30:24 +0100

    Pass :test_another_verse; interpolates argument in

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
-  def verse(number_of_verses)
+  def verse(verse_number)
     <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
+      #{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.
+      Take one down and pass it around, #{verse_number - 1} bottles of beer on the wall.
     VERSE
   end
 end
