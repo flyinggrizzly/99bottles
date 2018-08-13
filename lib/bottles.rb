df27d8982feaedd2113cb2cf8236cd0d46b6eb0a
Sean DMR <sean@flyinggrizzly.io>
2018-08-13 15:39:50 +0100

    Extract bottle/number string to helper
    Pass :test_verse_2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,16 @@
 class Bottles
   def verse(verse_number)
     <<~VERSE
-      #{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.
-      Take one down and pass it around, #{verse_number - 1} bottles of beer on the wall.
+      #{number_bottle_string(verse_number)} of beer on the wall, #{number_bottle_string(verse_number)} of beer.
+      Take one down and pass it around, #{number_bottle_string(verse_number - 1)} of beer on the wall.
     VERSE
+  end
+
+  def number_bottle_string(number)
+    if number == 1
+      '1 bottle'
+    else
+      "#{number} bottles"
+    end
   end
 end
