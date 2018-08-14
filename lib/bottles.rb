17af390957f416573f7dcd96cf07b6f119cddff5
Sean DMR <sean@flyinggrizzly.io>
2018-08-14 11:12:37 +0100

    2.3 rename unused param to descriptive name

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,5 @@
 class Bottles
-  def verse(_)
+  def verse(number)
     <<~VERSE
       99 bottles of beer on the wall, 99 bottles of beer.
       Take one down and pass it around, 98 bottles of beer on the wall.
