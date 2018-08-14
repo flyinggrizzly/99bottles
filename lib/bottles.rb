b87f94095e9b3d5279c89d14299e5664368dd138
Sean DMR <sean@flyinggrizzly.io>
2018-08-14 11:31:20 +0100

    2.5 pass test for second verse
    Prefers duplication over abstractions that make the situation less
    clear. The alternative would be to use a :pluralize method on 'bottle',
    but that could be a premature optimization. This code is pretty
    straightforward and easy to understand, even with the duplication.

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,9 +2,16 @@ class Bottles
   def verse(number)
     reduced_number = number - 1
 
-    <<~VERSE
-      #{number} bottles of beer on the wall, #{number} bottles of beer.
-      Take one down and pass it around, #{reduced_number} bottles of beer on the wall.
-    VERSE
+    if number == 2
+      <<~VERSE
+        2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall.
+      VERSE
+    else
+      <<~VERSE
+        #{number} bottles of beer on the wall, #{number} bottles of beer.
+        Take one down and pass it around, #{reduced_number} bottles of beer on the wall.
+      VERSE
+    end
   end
 end
