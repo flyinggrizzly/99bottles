090be0a809965f0223d1c81f7c798a44763a1fe7
Sean DMR <sean@flyinggrizzly.io>
2018-08-14 11:13:32 +0100

    2.3 consume params in template to generalize method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,10 @@
 class Bottles
   def verse(number)
+    reduced_number = number - 1
+
     <<~VERSE
-      99 bottles of beer on the wall, 99 bottles of beer.
-      Take one down and pass it around, 98 bottles of beer on the wall.
+      #{number} bottles of beer on the wall, #{number} bottles of beer.
+      Take one down and pass it around, #{reduced_number} bottles of beer on the wall.
     VERSE
   end
 end
