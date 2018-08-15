54811ef73f86c008e701ba55545d57884e5ccbf2
Sean DMR <sean@flyinggrizzly.io>
2018-08-15 11:33:45 +0100

    4.0 user number variable instead of hard-coded 1

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
     case number
     when 1
       <<~VERSE
-        1 bottle of beer on the wall, 1 bottle of beer.
+        #{number} bottle of beer on the wall, #{number} bottle of beer.
         Take it down and pass it around, no more bottles of beer on the wall.
       VERSE
     when 0
