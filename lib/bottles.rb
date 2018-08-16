5889ff94b74049ec99791355cf2c734972272088
Sean DMR <sean@flyinggrizzly.io>
2018-08-16 09:33:57 +0100

    4.8 use :successor in 0 case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -15,7 +15,7 @@ class Bottles
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
         action_for(number) +
-        "#{quantity(99)} #{container(number - 1)} of beer on the wall.\n"
+        "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
         "#{quantity(number)} #{container(number)} of beer.\n" +
