cbd86e168feb16a06cd59f43b3baeab7d6d6ccac
Sean DMR <sean@flyinggrizzly.io>
2018-08-16 13:40:13 +0100

    Work from Chapter 5
    5.2.3 add new BottleNumber class
    
    5.2.3 add initializer for BottleNumber
    
    5.2.3 ensure using BottleNumber doesn't cause issues in Bottles
    
    5.2.3 use only BottleNumber in Bottles#container
    
    5.2.3 replace private method logic in Bottles with BottleNumber calls
    
    5.2.4 rename unnecessary param temporarily
    
    5.2.4 stop sending unnecessary param
    
    5.2.4 remove defunct param from sig of BottleNumber#container
    
    5.2.4 remove params from BottleNumber#quantity signature
    
    5.2.4 remove params from sig of BottleNumber#action and rename it from action_for
    
    used :alias_method to keep method name available for callsites while
    renaming, similar to setting default argument
    
    5.2.4 remove params from sig of BottleNumber#successor
    
    5.2.4 remove pointless param from BottleNumber#pronoun signature
    
    5.2.4 remove unnecessary :pronoun method from Bottles
    
    5.6 reduce instantiations of BottleNumbers with local var
    
    5.6 remove more instantiations by creating next_bottle_number temp var
    
    5.6 remove now redundant Bottle methods

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,15 +10,24 @@ class Bottles
   end
 
   def verse(number)
-    "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{quantity(number)} #{container(number)} of beer.\n" +
-      action_for(number) +
-      "#{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+    bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
+
+    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, " +
+      "#{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
+      bottle_number.action +
+      "#{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+  end
+end
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
   end
 
-  private
-
-  def container(number)
+  def container
     if number == 1
       'bottle'
     else
@@ -26,7 +35,7 @@ class Bottles
     end
   end
 
-  def pronoun(number)
+  def pronoun
     if number == 1
       'it'
     else
@@ -34,7 +43,7 @@ class Bottles
     end
   end
 
-  def quantity(number)
+  def quantity
     if number.zero?
       'no more'
     else
@@ -42,15 +51,15 @@ class Bottles
     end
   end
 
-  def action_for(number)
+  def action
     if number.zero?
       "Go to the store and buy some more, "
     else
-      "Take #{pronoun(number)} down and pass it around, "
+      "Take #{pronoun} down and pass it around, "
     end
   end
 
-  def successor(number)
+  def successor
     if number.zero?
       99
     else
